49ac73bd2d141595fea2e5077d846f657ed0ec9a
Kevin Weyrowitz <kevinweyrowitz93@gmail.com>
2023-02-23 14:05:11 +0100

    fix issues

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // phew… not a lot going on here. Please add some code!
+const bookmark = document.querySelector(".bookmark");
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector(".card__button-answer");
+const answer = document.querySelector(".card__answer");
+
+answerButton.addEventListener("click", () => {
+  if (answer.classList.contains("card__answer") === true) {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
+  answer.classList.toggle("card__answer");
+});
